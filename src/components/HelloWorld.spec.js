63c2d747d657fa3efe8264f8895015a9cf164a7c
Liz Davidson <ldavidson@politico.com>
2021-12-29 12:57:59 -0600

    Set up initial vue app with jest and sass

---
+++ src/components/HelloWorld.spec.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from './HelloWorld.vue'
+
+describe('HelloWorld.vue', () => {
+  test('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
