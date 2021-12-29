a73a4989e0b1dde9f1062d4dbed3f14c12e26c58
Liz Davidson <ldavidson@politico.com>
2021-12-29 15:30:35 -0600

    Add start button that initializes the countdown timer functionality

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -10,6 +10,12 @@
 	{{ integer }}
 	</button>
 	</div>
+	<button class="keypad__start-button" 
+		@click="startTimer" 
+		:disabled="isStartDisabled"
+	>
+		Start
+	</button>
 
 	</div>
 </template>
@@ -21,6 +27,7 @@ export default {
 		return {
 			timerIntegerButtons: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
 			timerValues: [0, 0, 0, 0],
+			timerCountdown: null
 		}
 	},
 	computed: {
@@ -30,6 +37,14 @@ export default {
 		seconds() {
 			return `${this.timerValues[2]}${this.timerValues[3]}`
 		},
+		timerInMiliseconds() {
+			const minutes = this.timerValues[0] * 10 + this.timerValues[1]
+			const seconds = this.timerValues[2] * 10 + this.timerValues[3]
+			return minutes * 60000 + seconds * 1000
+		},
+		isStartDisabled() {
+			return this.timerInMiliseconds === 0 || this.timerCountdown
+		}
 	},
 	methods: {
 		updateTimer(valueToAdd) {
@@ -38,7 +53,28 @@ export default {
 			this.timerValues.push(valueToAdd)
 			this.timerValues.shift()
 		},
+		startTimer() {
+			this.timerCountdown = setInterval(this.subtractOneSecond, 1000)
+		},
+		subtractOneSecond() {
+			const [tenMins, oneMins, tenSec, oneSec] = this.timerValues
+			if (oneSec > 0) {
+				this.timerValues = [tenMins, oneMins, tenSec, oneSec - 1]
+			} else if (tenSec > 0) {
+				this.timerValues = [tenMins, oneMins, tenSec - 1, 9]
+			} else if (oneMins > 0) {
+				this.timerValues = [tenMins, oneMins - 1, 5, 9]
+			} else if (tenMins > 0) {
+				this.timerValues = [tenMins - 1, 9, 5, 9]
+			} else {
+				this.stopTimer()
+			}
+		},
+		stopTimer() {
+			clearInterval(this.timerCountdown)
+		}
 	}
 }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
